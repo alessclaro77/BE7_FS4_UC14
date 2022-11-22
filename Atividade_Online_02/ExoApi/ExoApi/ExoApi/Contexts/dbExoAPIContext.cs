cb40b36903a1258810adaae6a5cb21d97df42658
Alexclaro <alessclaro@gmail.com>
2022-11-22 16:04:02 -0400

    ER04 e AO02

---
+++ Atividade_Online_02/ExoApi/ExoApi/ExoApi/Contexts/dbExoAPIContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExoApi.Models;
+
+namespace ExoApi.Contexts
+{
+    public class dbExoAPIContext : DbContext
+    {
+        public dbExoAPIContext()
+        {
+        }
+        public dbExoAPIContext(DbContextOptions<dbExoAPIContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = LAPTOP-HBOR0GLL\\SQLEXPRESS; initial catalog = dbExoAPI; ; Integrated Security = true");
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
---
+++ Atividade_Online_02/script_exoapi.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE dbExoAPI;
+USE dbExoAPI;
+CREATE TABLE Projetos
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Titulo VARCHAR(150) NOT NULL,
+	Status VARCHAR(150) NOT NULL,
+	Data_De_Inicio VARCHAR(10) NOT NULL,
+	Area VARCHAR(150) NOT NULL
+);
+INSERT INTO Projetos (Titulo, Status, Data_De_Inicio, Area)
+VALUES ('Projeto A', 'Em Planejamento', '10/04/2022', 'T.I.');
+
+INSERT INTO Projetos (Titulo, Status, Data_De_Inicio, Area)
+VALUES ('Projeto B', 'Iniciado', '18/02/2022', 'RH');
+
+select * from Projetos;
+
+CREATE TABLE Usuarios(
+	Id INT PRIMARY KEY IDENTITY,
+	Email VARCHAR(255) NOT NULL UNIQUE,
+	Senha VARCHAR(120) NOT NULL,
+	Tipo CHAR(1) NOT NULL
+);
+
+INSERT INTO Usuarios VALUES ('teste@email.com.br','1234','0');
+
+SELECT * FROM Usuarios;
+
+SELECT * FROM Usuarios WHERE email = 'teste@email.com.br' AND senha='1234';
